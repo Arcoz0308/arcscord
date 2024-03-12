7554bc1dde348f3f685c08b4b799b6431632792b
Arcoz0308 <christopher@arcoz.dev>
2024-03-12 09:33:59 +0100

    Refactor command types to extend Command class
    The command types such as `SlashCommand`, `UserCommand`, and `MessageCommand` have been refactored to extend from the `Command` class. This reduces code redundancy and eases potential future modifications. Instead of defining each command type separately, they now merge the properties of the common Command class.

--- src/base/command/command.type.ts
+++ src/base/command/command.type.ts
@@ -3,21 +3,22 @@ import type { UserCommandBuilder } from "#/utils/discord/builder/user_command.cl
 import type { MessageCommandBuilder } from "#/utils/discord/builder/message_command.class";
 import type { UObject } from "#/utils/type/type";
 import type { SlashCmdBuilder } from "#/utils/discord/builder/slash_cmd.class";
+import type { Command } from "#/base/command/command.class";
 
 export type CommandRunContext<E extends UObject|null = null> = {
   interaction: ChatInputCommandInteraction;
   additionalInfos: E;
 }
 
-export type SlashCommand = {
+export type SlashCommand = Command & {
   slashBuilder: SlashCmdBuilder;
 }
 
-export type UserCommand = {
+export type UserCommand = Command & {
   userBuilder: UserCommandBuilder;
 }
 
-export type MessageCommand = {
+export type MessageCommand = Command & {
   messageBuilder: MessageCommandBuilder;
 }
 
