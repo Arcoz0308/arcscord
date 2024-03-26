5d18e962af6fd12831051b9a1152681d2845eb08
Arcoz0308 <christopher@arcoz.dev>
2024-03-26 10:09:42 +0100

    Implement InteractionError class
    A new InteractionError class has been created in the `interaction_error.class.ts` file. This subclass of BaseError includes additional details about an interaction in its debugging information, hence providing a more detailed error information specific to Discord interactions.

---
+++ src/utils/error/class/interaction_error.class.ts
@@ -0,0 +1,28 @@
+import { BaseError } from "#/utils/error/class/base_error.class";
+import type { DebugValueString, InteractionErrorOptions } from "#/utils/error/error.type";
+import type { BaseInteraction } from "discord.js";
+
+export class InteractionError extends BaseError {
+
+  interaction: BaseInteraction;
+
+  constructor(options: InteractionErrorOptions) {
+    super(options);
+
+    this.interaction = options.interaction;
+  }
+
+  getDebugsString(): DebugValueString[] {
+    const debugs: DebugValueString[] = [];
+    debugs.push(["author", `${this.interaction.user.username} (${this.interaction.user.id})`]);
+    if (this.interaction.inGuild()) {
+      debugs.push(["channel", `${this.interaction.channelId ?? "no channel"} (guild : ${this.interaction.guildId ?? "no guild"})`]);
+    } else {
+      debugs.push(["channel", "in dm"]);
+    }
+
+    debugs.push(...super.getDebugsString());
+    return debugs;
+  }
+
+}
