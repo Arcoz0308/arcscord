6927d5bc3a02d3d8e1542d207f9fcf72b62a46ad
Arcoz0308 <christopher@arcoz.dev>
2024-03-26 10:08:35 +0100

    Add command type conversion utility
    Introduced a new utility function `commandTypeToString` that converts different command types into string formats. This new utility will improve the readability and manageability of the code by replacing direct property checks with a more descriptive and self-explanatory method.

--- src/base/command/command.util.ts
+++ src/base/command/command.util.ts
@@ -1,5 +1,6 @@
 import type { MessageCommand, SlashCommand, UserCommand } from "#/base/command/command.type";
 import type { Command } from "#/base/command/command.class";
+import { ApplicationCommandType } from "discord-api-types/v10";
 
 export const isSlashCommand = (cmd: Command): cmd is SlashCommand => {
   return "slashBuilder" in cmd;
@@ -11,4 +12,17 @@ export const isUserCommand = (cmd: Command): cmd is UserCommand => {
 
 export const isMessageCommand = (cmd: Command): cmd is MessageCommand => {
   return "messageBuilder" in cmd;
+};
+
+export const commandTypeToString = (type: ApplicationCommandType): string => {
+  switch (type) {
+    case ApplicationCommandType.ChatInput:
+      return "Slash Command";
+    case ApplicationCommandType.Message:
+      return "Message Command";
+    case ApplicationCommandType.User:
+      return "User Command";
+    default:
+      return "Unknown Command type";
+  }
 };
