91ecdad391033fe69d3f05b6c78c8e202dbb5c06
Arcoz0308 <christopher@arcoz.dev>
2024-03-26 10:52:22 +0100

    Enhance error handling by adding context and command to CommandErrorOptions
    The 'CommandError' class now includes 'CommandRunContext' and 'Command' (including 'SubCommand') in its options. This provides more context for error handling and improves interaction management within commands, offering greater flexibility and precision.

--- src/utils/error/class/command_error.class.ts
+++ src/utils/error/class/command_error.class.ts
@@ -5,21 +5,34 @@ import type {
   UserContextMenuCommandInteraction
 } from "discord.js";
 import type { CommandErrorOptions, DebugValueString } from "#/utils/error/error.type";
-import { commandTypeToString } from "#/base/command";
+import type { Command, CommandRunContext } from "#/base/command";
+import { commandTypeToString, subCommandNameToString } from "#/base/command";
+import { SubCommand } from "#/base/sub_command/sub_command.class";
 
 export class CommandError extends InteractionError {
 
   interaction: ChatInputCommandInteraction|MessageContextMenuCommandInteraction|UserContextMenuCommandInteraction;
 
+  context: CommandRunContext;
+
+  command: Command|SubCommand;
+
   constructor(options: CommandErrorOptions) {
     super(options);
 
     this.interaction = options.interaction;
+    this.context = options.context;
+    this.command = options.command;
   }
 
   getDebugsString(): DebugValueString[] {
     const debugs: DebugValueString[] = [];
-    debugs.push(["command name", this.interaction.commandName]);
+    if (this.command instanceof SubCommand) {
+      debugs.push(["Command name", subCommandNameToString(this.command)]);
+    } else {
+      debugs.push(["Command name", this.interaction.commandName]);
+    }
+
     debugs.push(["Command type", commandTypeToString(this.interaction.commandType)]);
 
     debugs.push(...super.getDebugsString());
