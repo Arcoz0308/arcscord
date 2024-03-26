8e58dcac88f726a66d2a680512341eaee36c9e6c
Arcoz0308 <christopher@arcoz.dev>
2024-03-26 10:08:56 +0100

    Extend error options in error.type.ts
    Added more specific `InteractionErrorOptions` and `CommandErrorOptions` for better error handling and distinction. This enhancement allows more detail oriented interaction error reporting, thereby improving the robustness and maintainability of the application.

--- src/utils/error/error.type.ts
+++ src/utils/error/error.type.ts
@@ -1,4 +1,10 @@
 import type { BaseError } from "#/utils/error/class/base_error.class";
+import type {
+  BaseInteraction,
+  ChatInputCommandInteraction,
+  MessageContextMenuCommandInteraction,
+  UserContextMenuCommandInteraction
+} from "discord.js";
 
 export type ErrorOptions = {
   message?: string;
@@ -8,3 +14,11 @@ export type ErrorOptions = {
 
 export type DebugValues = {[key: string]: unknown};
 export type DebugValueString = [key: string, value: string];
+
+export type InteractionErrorOptions = ErrorOptions & {
+  interaction: BaseInteraction;
+}
+
+export type CommandErrorOptions = InteractionErrorOptions & {
+  interaction: ChatInputCommandInteraction|MessageContextMenuCommandInteraction|UserContextMenuCommandInteraction;
+}
