6e2f87eebbce3300b03ee5c8a8c8e27e89a95e3b
Arcoz0308 <christopher@arcoz.dev>
2024-03-27 21:48:10 +0100

    Add fullName method to SubCommand class
    Implemented a new method 'fullName' in the SubCommand class, which is used to get the full name of a command. This method concatenates the base command name, subgroup name (if any), and the sub command name, enhancing the command handling process.

--- src/base/sub_command/sub_command.class.ts
+++ src/base/sub_command/sub_command.class.ts
@@ -20,4 +20,11 @@ export abstract class SubCommand extends Command {
     this.name = this.baseCommand.name;
   }
 
+  fullName(): string {
+    if (this.subGroup !== null) {
+      return `${this.name}-${this.subGroup}-${this.subName}`;
+    }
+    return `${this.name}-${this.subName}`;
+  }
+
 }
