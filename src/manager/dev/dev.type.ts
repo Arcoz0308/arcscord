65c1869aefa57516d7d0e22a9fd4b353a79e5289
Arcoz0308 <christopher@arcoz.dev>
2024-03-27 21:13:15 +0100

    Add new types in dev.type.ts file
    The dev.type.ts file has been updated to improve error handling and interaction management. New types 'DevConfig' and 'DevConfigKey' derived from 'devConfigSchema' and 'keyof' respectively have been introduced. Also, import statements for 'z' from "zod" and 'devConfigSchema' were added.

--- src/manager/dev/dev.type.ts
+++ src/manager/dev/dev.type.ts
@@ -1,3 +1,9 @@
+import type { z } from "zod";
+import type { devConfigSchema } from "#/manager/dev/dev.z";
+
 export type DevFacultative = {
   isEnableInDev: boolean;
 }
+
+export type DevConfig = z.infer<typeof devConfigSchema>
+export type DevConfigKey = keyof DevConfig;
