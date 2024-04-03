86a84014301dec2fed22b897829b588874cf195e
Arcoz0308 <christopher@arcoz.dev>
2024-04-03 15:07:22 +0200

    Enhance debug message formatting in logger utility
    Added new constants and methods that refine color coding and message formatting in logger utility. The changes make debug messages clearer and more comprehensible, improving the debugging process significantly.

--- src/utils/logger/logger.util.ts
+++ src/utils/logger/logger.util.ts
@@ -2,14 +2,19 @@ import type { LogLevel } from "#/utils/logger/logger.type";
 import { effectReset } from "tintify";
 import {
   DATE_COLOR,
+  DEBUG_KEY_COLOR,
+  DEBUG_VALUE_COLOR,
   logLevelInfos,
   MAX_PROCESS_LENGTH,
   MAX_TITLE_LENGTH,
   PROCESS_NAME_COLOR,
   SEPARATOR_COLOR,
+  SHORT_DEBUG_PREFIX,
+  SHORT_DEBUG_SPACING,
   SPACE_FILLER
 } from "#/utils/logger/logger.const";
 import { DayJS } from "#/utils/dayjs/dayjs";
+import type { DebugValueString } from "#/utils/error/error.type";
 
 export const formatLog = (logLevel: LogLevel, message: string, processName = "main"): string => {
   const reset = effectReset.all;
@@ -26,6 +31,27 @@ export const formatLog = (logLevel: LogLevel, message: string, processName = "ma
 
   const prefix = `${reset}${DATE_COLOR}[${date}] ${reset}${PROCESS_NAME_COLOR}${processName}`;
   const middle = `${reset} ${SEPARATOR_COLOR}[${options.titleColor}${options.logText}${reset}${SEPARATOR_COLOR}]`;
+  const separator = `${reset} ${SEPARATOR_COLOR}${"-".repeat(MAX_TITLE_LENGTH - options.logText.length)} » `;
+  const text = `${reset}${options.textColor}${message}`;
+
+  return prefix + middle + separator + text;
+};
+
+export const colorDebugValue = (debug: DebugValueString): string => {
+  const reset = effectReset.all;
+  return `${reset}${DEBUG_KEY_COLOR}${debug[0]} : ${reset}${DEBUG_VALUE_COLOR}${debug[1]}`;
+};
+
+export const formatShortDebug = (message: string|DebugValueString): string => {
+  if (typeof message !== "string") {
+    message = colorDebugValue(message);
+  }
+
+  const options = logLevelInfos["debug"];
+  const reset = effectReset.all;
+
+  const prefix = `${reset}${SEPARATOR_COLOR}${" ".repeat(SHORT_DEBUG_SPACING)}${SHORT_DEBUG_PREFIX}`;
+  const middle = `[${reset}${options.titleColor}${options.logText}${reset}${SEPARATOR_COLOR}]`;
   const separator = `${reset} ${SEPARATOR_COLOR}${"-".repeat(MAX_TITLE_LENGTH - options.logText.length)} » `;
   const text = `${reset}${options.textColor}${message}`;
 
