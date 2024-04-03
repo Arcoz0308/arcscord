a929796425871dc51e0780788def397695c402b8
Arcoz0308 <christopher@arcoz.dev>
2024-04-03 19:19:52 +0200

    Update Client class adding logger, intents, and REST

--- src/base/client/client.class.ts
+++ src/base/client/client.class.ts
@@ -1,11 +1,37 @@
-import { Client as DJSClient } from "discord.js";
+import { Client as DJSClient, IntentsBitField, REST } from "discord.js";
 import { CommandManager } from "#/manager/command/command_manager.class";
 import { DevManager } from "#/manager/dev";
+import { logger } from "#/utils/logger/logger.class";
 
 export class Client extends DJSClient {
 
   commandManager = new CommandManager(this);
 
   devManager = new DevManager();
+
+  logger = logger;
+
+  rest: REST;
+
+  constructor(token: string) {
+    super({
+      intents: [
+        IntentsBitField.Flags.GuildScheduledEvents,
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+        IntentsBitField.Flags.DirectMessages,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildVoiceStates,
+        IntentsBitField.Flags.GuildPresences,
+      ],
+    });
+
+    this.token = token;
+
+    this.rest = new REST({
+      version: "10",
+    }).setToken(token);
+  }
 
 }
