df08bcb178b781d5d45e6a1e5a1dd10f9789a8f2
Arcoz0308 <christopher@arcoz.dev>
2024-04-03 19:19:08 +0200

    Added 'config' object to 'dev.z.ts'

--- src/manager/dev/dev.z.ts
+++ src/manager/dev/dev.z.ts
@@ -4,4 +4,7 @@ export const devConfigSchema = z.object({
   commands: z.array(z.string()),
   events: z.array(z.string()),
   tasks: z.array(z.string()),
+  config: z.object({
+    devGuildId: z.string(),
+  }),
 });
