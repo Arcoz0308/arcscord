cec1e999f5a8ffcd8730122a4eae03058c722aa4
Arcoz0308 <christopher@arcoz.dev>
2024-04-04 20:52:12 +0200

    Added error ID generation to InteractionError class

--- src/utils/error/class/interaction_error.class.ts
+++ src/utils/error/class/interaction_error.class.ts
@@ -1,10 +1,13 @@
 import { BaseError } from "#/utils/error/class/base_error.class";
 import type { DebugValueString, InteractionErrorOptions } from "#/utils/error/error.type";
 import type { BaseInteraction } from "discord.js";
+import ShortUniqueId from "short-unique-id";
 
 export class InteractionError extends BaseError {
 
   interaction: BaseInteraction;
+
+  id?: string;
 
   constructor(options: InteractionErrorOptions) {
     super(options);
@@ -15,14 +18,24 @@ export class InteractionError extends BaseError {
   getDebugsString(): DebugValueString[] {
     const debugs: DebugValueString[] = [];
     debugs.push(["author", `${this.interaction.user.username} (${this.interaction.user.id})`]);
+
     if (this.interaction.inGuild()) {
       debugs.push(["channel", `${this.interaction.channelId ?? "no channel"} (guild : ${this.interaction.guildId ?? "no guild"})`]);
     } else {
       debugs.push(["channel", "in dm"]);
     }
 
+    if (this.id) {
+      debugs.push(["id", this.id]);
+    }
+
     debugs.push(...super.getDebugsString());
     return debugs;
+  }
+
+  generateId(): this {
+    this.id = new ShortUniqueId().stamp(16);
+    return this;
   }
 
 }
