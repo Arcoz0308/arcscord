e1ad1576d6bf27d05e76c2e24410d539200eac08
Arcoz0308 <christopher@arcoz.dev>
2024-04-06 23:59:08 +0200

    Add new task types enum and corresponding type definition

---
+++ src/base/task/task.enum.ts
@@ -0,0 +1,5 @@
+export const taskTypes = {
+  cron: "cron",
+  delay: "delay",
+  multiCron: "multiCron",
+};
---
+++ src/base/task/task.type.ts
@@ -0,0 +1,3 @@
+import type { taskTypes } from "#/base/task/task.enum";
+
+export type TaskType = keyof typeof taskTypes;
