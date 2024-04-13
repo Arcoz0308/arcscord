b45cb00797543fd1142af59daff4eaacb7282514
Arcoz0308 <christopher@arcoz.dev>
2024-04-13 09:12:41 +0200

    Add SelectMenuError class in utils/error/class

---
+++ src/utils/error/class/select_menu_error.class.ts
@@ -0,0 +1,26 @@
+import { ComponentError } from "#/utils/error/class/component_error.class";
+import type { AnySelectMenuInteraction } from "discord.js";
+import type { DebugValueString, SelectMenuErrorOptions } from "#/utils/error/error.type";
+
+export class SelectMenuError extends ComponentError {
+
+  interaction: AnySelectMenuInteraction;
+
+  constructor(options: SelectMenuErrorOptions) {
+    super(options);
+
+    this.interaction = options.interaction;
+  }
+
+  getDebugsString(): DebugValueString[] {
+
+    const debugs: DebugValueString[] = [];
+    debugs.push(["selectValues", `"${this.interaction.values.join("\", \"")}"`]);
+
+
+    debugs.push(...super.getDebugsString());
+
+    return debugs;
+  }
+
+}
