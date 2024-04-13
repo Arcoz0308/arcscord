4f23feb1b37307b9316dfed890b0c6a7bab69e73
Arcoz0308 <christopher@arcoz.dev>
2024-04-13 09:30:24 +0200

    Add abstract SelectMenu class in message_component module

---
+++ src/base/message_component/select_menu/select_menu.class.ts
@@ -0,0 +1,8 @@
+import { BaseComponent } from "#/base/message_component/base/base_component.class";
+import type { ComponentType } from "#/base/message_component/base/base_component.type";
+
+export abstract class SelectMenu extends BaseComponent {
+
+  type: ComponentType = "selectMenu";
+
+}
