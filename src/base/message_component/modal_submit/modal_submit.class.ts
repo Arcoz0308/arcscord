038faa3b1d7a3605a5cc63c81fbe648e3a66c46f
Arcoz0308 <christopher@arcoz.dev>
2024-04-14 22:37:27 +0200

    Add new ModalSubmitTypes and update ModalSubmitClass with run method

--- src/base/message_component/modal_submit/modal_submit.class.ts
+++ src/base/message_component/modal_submit/modal_submit.class.ts
@@ -1,11 +1,15 @@
 import { BaseComponent } from "#/base/message_component/base/base_component.class";
 import type { ComponentBuilderField, ComponentType } from "#/base/message_component/base/base_component.type";
 import type { ModalBuilder } from "@discordjs/builders";
+import type { ModalSubmitRunContext } from "#/base/message_component/modal_submit/modal_submit.type";
+import type { ButtonRunResult } from "#/base/message_component/button/button.type";
 
 export abstract class ModalSubmitComponent extends BaseComponent {
 
   type: ComponentType = "modalSubmit";
 
   abstract builder: ComponentBuilderField<ModalBuilder>
+
+  abstract run(ctx: ModalSubmitRunContext): Promise<ButtonRunResult>;
 
 }
---
+++ src/base/message_component/modal_submit/modal_submit.type.ts
@@ -0,0 +1,10 @@
+import type { ModalSubmitInteraction } from "discord.js";
+import type { Result } from "#/utils/error/error.type";
+import type { ModalSubmitError } from "#/utils/error/class/modal_submit_error.class";
+
+export type ModalSubmitRunContext = {
+  interaction: ModalSubmitInteraction;
+  defer: boolean;
+}
+
+export type ModalSubmitRunResult = Result<true|string, ModalSubmitError>
