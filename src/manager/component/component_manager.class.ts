86df36a66e43f14d1ebdeb7b88a85a445640b24a
Arcoz0308 <christopher@arcoz.dev>
2024-04-15 20:54:10 +0200

    Add button, select menu, and modal submit components to ComponentManager

--- src/manager/component/component_manager.class.ts
+++ src/manager/component/component_manager.class.ts
@@ -1,8 +1,17 @@
 import { BaseManager } from "#/base/manager/manager.class";
+import type { ButtonComponent } from "#/base/message_component/button/button.class";
+import type { SelectMenu } from "#/base/message_component/select_menu/select_menu.class";
+import type { ModalSubmitComponent } from "#/base/message_component/modal_submit/modal_submit.class";
 
 export class ComponentManager extends BaseManager {
 
   name = "components";
+
+  buttons: Map<string, ButtonComponent> = new Map();
+
+  selectMenus: Map<string, SelectMenu> = new Map();
+
+  modalSubmit: Map<string, ModalSubmitComponent> = new Map();
 
 
 }
