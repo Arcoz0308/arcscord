5e624b2652ef0efcc85e894bf1c7de1a3ebac7ff
Arcoz0308 <christopher@arcoz.dev>
2024-04-16 09:43:30 +0200

    Remove specific select menu types and associated methods in select_menu.class.ts

--- src/base/message_component/select_menu/select_menu.class.ts
+++ src/base/message_component/select_menu/select_menu.class.ts
@@ -1,14 +1,6 @@
 import { BaseComponent } from "#/base/message_component/base/base_component.class";
 import type { ComponentType } from "#/base/message_component/base/base_component.type";
-import type {
-  AnySelectMenuClass,
-  ChannelSelectMenuClass,
-  MentionableSelectMenuClass,
-  RoleSelectMenuClass,
-  StringSelectMenuClass,
-  UserSelectMenuClass
-} from "#/base/message_component/select_menu/select_menu.type";
-import { ComponentType as DJSComponentType } from "discord-api-types/v10";
+import type { AnySelectMenuClass } from "#/base/message_component/select_menu/select_menu.type";
 
 export abstract class SelectMenu<T extends AnySelectMenuClass> extends BaseComponent {
 
@@ -19,26 +11,6 @@ export abstract class SelectMenu<T extends AnySelectMenuClass> extends BaseCompo
   authorOnly = false;
 
   abstract run(ctx: Parameters<T["run"]>[0]): ReturnType<T["run"]>
-
-  isChannelSelect(): this is ChannelSelectMenuClass {
-    return this.builder.data.type as DJSComponentType === DJSComponentType.ChannelSelect;
-  }
-
-  isMentionableSelect(): this is MentionableSelectMenuClass {
-    return this.builder.data.type as DJSComponentType === DJSComponentType.MentionableSelect;
-  }
-
-  isRoleSelect(): this is RoleSelectMenuClass {
-    return this.builder.data.type as DJSComponentType === DJSComponentType.RoleSelect;
-  }
-
-  isStringSelect(): this is StringSelectMenuClass {
-    return this.builder.data.type as DJSComponentType === DJSComponentType.StringSelect;
-  }
-
-  isUserSelect(): this is UserSelectMenuClass {
-    return this.builder.data.type as DJSComponentType === DJSComponentType.UserSelect;
-  }
 
 
 }
