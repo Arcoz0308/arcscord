79a59bd7a0cbb6819c00c6435f85c18e218ae8f9
Arcoz0308 <christopher@arcoz.dev>
2024-04-16 08:29:38 +0200

    Refactor component manager and add component loading methods

--- src/manager/component/component_manager.class.ts
+++ src/manager/component/component_manager.class.ts
@@ -1,7 +1,9 @@
 import { BaseManager } from "#/base/manager/manager.class";
-import type { ButtonComponent } from "#/base/message_component/button/button.class";
-import type { SelectMenu } from "#/base/message_component/select_menu/select_menu.class";
-import type { ModalSubmitComponent } from "#/base/message_component/modal_submit/modal_submit.class";
+import { ButtonComponent } from "#/base/message_component/button/button.class";
+import { ModalSubmitComponent } from "#/base/message_component/modal_submit/modal_submit.class";
+import type { AnySelectMenu } from "#/base/message_component/select_menu/select_menu.type";
+import type { Component } from "#/base/message_component/base/base_component.type";
+import { SelectMenu } from "#/base/message_component/select_menu/select_menu.class";
 
 export class ComponentManager extends BaseManager {
 
@@ -9,9 +11,35 @@ export class ComponentManager extends BaseManager {
 
   buttons: Map<string, ButtonComponent> = new Map();
 
-  selectMenus: Map<string, SelectMenu> = new Map();
+  selectMenus: Map<string, AnySelectMenu> = new Map();
 
   modalSubmit: Map<string, ModalSubmitComponent> = new Map();
+
+  loadComponent(component: Component): void {
+    if (component.type === "button" && component instanceof ButtonComponent) {
+      return this.loadButton(component);
+    }
+
+    if (component.type === "modalSubmit" && component instanceof ModalSubmitComponent) {
+      return this.loadModalSubmit(component);
+    }
+
+    if (component.type === "selectMenu" && component instanceof SelectMenu) {
+      return this.loadSelectMenu(component);
+    }
+  }
+
+  loadButton(component: ButtonComponent): void {
+
+  }
+
+  loadModalSubmit(component: ModalSubmitComponent): void {
+
+  }
+
+  loadSelectMenu(component: AnySelectMenu): void {
+
+  }
 
 
 }
