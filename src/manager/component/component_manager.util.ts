29c38bed533a7dc98d418c4e9d9b5625662d7db2
Arcoz0308 <christopher@arcoz.dev>
2024-04-17 20:09:57 +0200

    Rename function `Components` to `getComponents` in component manager utility.

--- src/manager/component/component_manager.util.ts
+++ src/manager/component/component_manager.util.ts
@@ -1,6 +1,6 @@
 import type { Client } from "#/base/client/client.class";
 import type { Component } from "#/base/message_component/base/base_component.type";
 
-export const Components = (client: Client): Component[] => [
+export const getComponents = (client: Client): Component[] => [
 
 ];
