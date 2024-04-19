7d20f6b394850d140fb777e13ccb5306af6cdc7f
Arcoz0308 <christopher@arcoz.dev>
2024-04-19 20:52:07 +0200

    Add 'authorOnly' embed for author-restricted commands

--- src/utils/discord/embed/embed.const.ts
+++ src/utils/discord/embed/embed.const.ts
@@ -6,3 +6,10 @@ export const internalErrorEmbed = (id?: string):  EmbedBuilder => {
     .setTitle("internalError")
     .setDescription("a internal error happened !" + id ? `\nError id : \`${id}\`` : "");
 };
+
+export const authorOnly = (): EmbedBuilder => {
+  return new EmbedBuilder()
+    .setColor("#ff0000")
+    .setTitle("Author Only")
+    .setDescription("This command can only be used by the author of the command.");
+};
