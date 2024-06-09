4cb7292d0eaec727ef0c96cdbcc00cfaee72efe8
Arcoz0308 <christopher@arcoz.dev>
2024-06-09 16:28:47 +0200

    Remove global commands and their related builders from arcscord package.

--- packages/arcscord/src/commands/global/info/info.builder.ts
+++
@@ -1,5 +0,0 @@
-import { SlashCmdBuilder } from "#/utils/discord/builder/slash_cmd.class";
-
-export const infoSlashBuilder = new SlashCmdBuilder()
-  .setName("info")
-  .setDescription("get infos about the bot");
--- packages/arcscord/src/manager/command/command_manager.util.ts
+++
@@ -1,9 +0,0 @@
-import type { Command } from "#/base/command/command.class";
-import { InfoCommand } from "#/commands/global/info/info.class";
-import type { ArcClient } from "#/base/client/client.class";
-import { UserInfoCommand } from "#/commands/global/user_info/user_info.class";
-
-export const globalCommands = (client: ArcClient): Command[] => [
-  new InfoCommand(client),
-  new UserInfoCommand(client),
-];
