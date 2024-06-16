8ca4051c41a9c5b24b47a7be9abfa0bbf1254b34
Arcoz0308 <christopher@arcoz.dev>
2024-06-16 18:03:31 +0200

    Add reply and editReply methods to ModalSubmitComponent class

--- packages/arcscord/src/base/message_component/modal_submit/modal_submit.class.ts
+++ packages/arcscord/src/base/message_component/modal_submit/modal_submit.class.ts
@@ -1,8 +1,12 @@
 import { BaseComponent } from "#/base/message_component/base/base_component.class";
 import type { ComponentBuilderField, ComponentType } from "#/base/message_component/base/base_component.type";
 import type { ModalBuilder } from "@discordjs/builders";
-import type { ModalSubmitRunContext } from "#/base/message_component/modal_submit/modal_submit.type";
-import type { ButtonRunResult } from "#/base/message_component/button/button.type";
+import type {
+  ModalSubmitRunContext,
+  ModalSubmitRunResult
+} from "#/base/message_component/modal_submit/modal_submit.type";
+import type { InteractionEditReplyOptions, InteractionReplyOptions, MessagePayload } from "discord.js";
+import { anyToError, error, ModalSubmitError, ok } from "#/utils";
 
 export abstract class ModalSubmitComponent extends BaseComponent {
 
@@ -10,6 +14,33 @@ export abstract class ModalSubmitComponent extends BaseComponent {
 
   abstract builder: ComponentBuilderField<ModalBuilder>
 
-  abstract run(ctx: ModalSubmitRunContext): Promise<ButtonRunResult>;
+  abstract run(ctx: ModalSubmitRunContext): Promise<ModalSubmitRunResult>;
+
+
+  async reply(ctx: ModalSubmitRunContext,  message: string | MessagePayload | InteractionReplyOptions): Promise<ModalSubmitRunResult> {
+    try {
+      await ctx.interaction.reply(message);
+      return ok(true);
+    } catch (e) {
+      return error(new ModalSubmitError({
+        interaction: ctx.interaction,
+        message: `failed to reply to interaction : ${anyToError(e).message}`,
+        baseError: anyToError(e),
+      }));
+    }
+  }
+
+  async editReply(ctx: ModalSubmitRunContext,  message: string | MessagePayload | InteractionEditReplyOptions): Promise<ModalSubmitRunResult> {
+    try {
+      await ctx.interaction.editReply(message);
+      return ok(true);
+    } catch (e) {
+      return error(new ModalSubmitError({
+        interaction: ctx.interaction,
+        message: `failed to edit reply to interaction : ${anyToError(e).message}`,
+        baseError: anyToError(e),
+      }));
+    }
+  }
 
 }
