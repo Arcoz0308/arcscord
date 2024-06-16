adc2f38d7fa71907e0eab40fb0775ec6be6e63ae
Arcoz0308 <christopher@arcoz.dev>
2024-06-16 18:02:08 +0200

    Add reply and editReply methods to Button class

--- packages/arcscord/src/base/message_component/button/button.class.ts
+++ packages/arcscord/src/base/message_component/button/button.class.ts
@@ -2,6 +2,8 @@ import { BaseComponent } from "#/base/message_component/base/base_component.clas
 import type { ComponentBuilderField, ComponentType } from "#/base/message_component/base/base_component.type";
 import type { ButtonBuilder } from "@discordjs/builders";
 import type { ButtonRunContext, ButtonRunResult } from "#/base/message_component/button/button.type";
+import type { InteractionEditReplyOptions, InteractionReplyOptions, MessagePayload } from "discord.js";
+import { anyToError, ButtonError, error, ok } from "#/utils";
 
 export abstract class Button extends BaseComponent {
 
@@ -12,5 +14,32 @@ export abstract class Button extends BaseComponent {
   authorOnly = false;
 
   abstract run(ctx: ButtonRunContext): Promise<ButtonRunResult>
+
+
+  async reply(ctx: ButtonRunContext,  message: string | MessagePayload | InteractionReplyOptions): Promise<ButtonRunResult> {
+    try {
+      await ctx.interaction.reply(message);
+      return ok(true);
+    } catch (e) {
+      return error(new ButtonError({
+        interaction: ctx.interaction,
+        message: `failed to reply to interaction : ${anyToError(e).message}`,
+        baseError: anyToError(e),
+      }));
+    }
+  }
+
+  async editReply(ctx: ButtonRunContext,  message: string | MessagePayload | InteractionEditReplyOptions): Promise<ButtonRunResult> {
+    try {
+      await ctx.interaction.editReply(message);
+      return ok(true);
+    } catch (e) {
+      return error(new ButtonError({
+        interaction: ctx.interaction,
+        message: `failed to edit reply to interaction : ${anyToError(e).message}`,
+        baseError: anyToError(e),
+      }));
+    }
+  }
 
 }
