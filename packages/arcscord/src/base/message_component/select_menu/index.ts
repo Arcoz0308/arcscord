d2be45e86f9261514c586d61bda6edacefddfa55
Arcoz0308 <christopher@arcoz.dev>
2024-06-16 20:12:53 +0200

    Fix spacing in export type statements for consistency

--- packages/arcscord/src/base/message_component/select_menu/index.ts
+++ packages/arcscord/src/base/message_component/select_menu/index.ts
@@ -1,2 +1,2 @@
 export { SelectMenu } from "./select_menu.class";
-export type { SelectMenuRunResult, SelectMenuRunContext} from "./select_menu.type";
+export type { SelectMenuRunResult, SelectMenuRunContext } from "./select_menu.type";
