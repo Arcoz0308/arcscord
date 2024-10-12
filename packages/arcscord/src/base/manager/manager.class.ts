87e895878401fb2440f08cc2e773fd48900d6289
Chris <christopher@arcoz.dev>
2024-10-12 19:11:29 +0200

    Add detailed JSDoc comments to BaseManager class and its methods

--- packages/arcscord/src/base/manager/manager.class.ts
+++ packages/arcscord/src/base/manager/manager.class.ts
@@ -1,17 +1,38 @@
 import type { ArcClient } from "#/base/client/client.class";
 import type { LoggerInterface } from "#/utils/logger/logger.type";
 
+/**
+ * Abstract class representing a base manager that all other managers should extend.
+ */
 export abstract class BaseManager {
+  /**
+   * The client instance
+   */
   client: ArcClient;
 
+  /**
+   * The name of the manager. Should be defined by subclasses.
+   */
   abstract name: string;
 
+  /**
+   * Constructs a new instance of the BaseManager.
+   *
+   * @param client - The ArcClient instance.
+   */
   constructor(client: ArcClient) {
     this.client = client;
   }
 
+  /**
+   * @internal
+   */
   _logger?: LoggerInterface;
 
+  /**
+   * Retrieves the logger associated with the manager.
+   * If no logger exists, a new one is created using the client's logger constructor.
+   */
   get logger(): LoggerInterface {
     if (!this._logger) {
       this._logger = this.client.createLogger(this.name);
