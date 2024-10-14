9f05f89e876fed0f15cd7107d448d62d760f60f2
Williams Bussat <github@wbussat.fr>
2024-10-14 14:45:12 +0200

    build: improve tsup config to split esm and cjs builds

--- packages/arcscord/tsup.config.ts
+++ packages/arcscord/tsup.config.ts
@@ -1,12 +1,3 @@
-import { defineConfig } from "tsup";
+import { createTsupConfig } from "../../scripts/tsup.config";
 
-export default defineConfig({
-  entry: ["src/index.ts", "!src/**/__tests__/**", "!src/**/*.test.*", "!test"],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-  target: "es2022",
-  platform: "node",
-  format: ["esm", "cjs"],
-  dts: true,
-});
+export default createTsupConfig({});
