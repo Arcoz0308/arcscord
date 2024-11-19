9fa8976983b897a7411d3934c6fef1887cc457e7
Chris <christopher@arcoz.dev>
2024-11-19 23:02:11 +0100

    chore: remove deprecated locale type and resource files

--- packages/arcscord/src/@types/i18next.d.ts
+++
@@ -1,9 +0,0 @@
-import type resources from "#/@types/recources";
-
-declare module "i18next" {
-  // eslint-disable-next-line ts/consistent-type-definitions
-  interface CustomTypeOptions {
-    defaultNS: "empty";
-    resources: typeof resources;
-  }
-}
--- packages/arcscord/src/@types/recources.ts
+++
@@ -1,9 +0,0 @@
-import arcscord from "../locales/en.json";
-
-const resources = {
-  arcscord,
-  // for forcing use arcscord namespace but typed correctly
-  empty: {},
-} as const;
-
-export default resources;
